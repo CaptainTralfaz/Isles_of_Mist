2a2a9fd7c863de159b4df2768c0e2327fc980e24
tralfaz <bhaler@gmail.com>
2020-08-25 15:42:15 -0700

    after part 3 more tiles, entity placement (including water flood-fill)

--- src/procgen.py
+++ src/procgen.py
@@ -1,13 +1,17 @@
 from src.game_map import GameMap
-from src.tile import Elevation, Terrain
-
-
-from random import randint, seed
+from src.tile import Elevation
+from typing import Set, List, Tuple
+from queue import Queue
+from random import randint, choice
 from math import pow
 from opensimplex import OpenSimplex
+from src.entity import Entity
+from src.utilities import get_hex_water_neighbors
+
+tile_size = 32
 
 
-def generate_map(map_width, map_height) -> GameMap:
+def generate_map(map_width, map_height, entities) -> GameMap:
     island_map = GameMap(map_width, map_height)
     # noise_map = [[0.0 for y in range(map_height)] for x in range(map_width)]
     
@@ -57,10 +61,47 @@ def generate_map(map_width, map_height) -> GameMap:
                 island_map.terrain[x][y] = Elevation.MOUNTAIN
             else:
                 island_map.terrain[x][y] = Elevation.VOLCANO
-
+    
+    place_entities(island_map, entities)
+    
     return island_map
 
 
 def noise(gen, nx, ny):
     # Rescale from -1.0:+1.0 to 0.0:1.0
     return gen.noise2d(nx, ny) / 2.0 + 0.5
+
+
+def place_entities(game_map: GameMap, entities: Set[Entity]) -> None:
+    water = explore_water_iterative(game_map, 0, 0)
+    for entity in entities:
+        water_tile = choice(water)
+        water.remove(water_tile)
+        entity.x = water_tile[0] * tile_size
+        entity.y = water_tile[1] * tile_size
+        entity.facing = randint(0, 5)
+
+
+def explore_water_iterative(game_map: GameMap, x: int, y: int) -> List[Tuple[int, int]]:
+    """
+    Finds all "islands" on the game map. "islands" are sets of adjacent land tiles. "land tiles" have elevation > 2
+    :param game_map: GameMap
+    :param x: int x coordinate
+    :param y: int y coordinate
+    :return: list of tile coordinates
+    """
+    frontier = Queue()
+    frontier.put((x, y))
+    visited = [(x, y)]
+    
+    while not frontier.empty():
+        current = frontier.get()
+        x, y = current
+        for neighbor in get_hex_water_neighbors(game_map=game_map, x=x, y=y):
+            if neighbor not in visited:
+                frontier.put(neighbor)
+                visited.append(neighbor)
+    return visited
+
+
+
